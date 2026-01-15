583b953f90aa682c5c85c0e0dc96085bbabf768f
Butkova Inna <inna-5675@mail.ru>
2026-01-15 10:27:52 +0500

    Исправлено ДЗ5

--- AstonLearning_5/src/Main.java
+++ AstonLearning_5/src/Main.java
@@ -1,64 +1,98 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
     public static void main(String[] args) {
-        String [][] correctArray = {
+        String[][] array = {
                 {"1", "2", "3", "4"},
                 {"5", "6", "7", "8"},
                 {"9", "10", "11", "12"},
                 {"13", "14", "15", "16"}
         };
-        String [][] incorrectArray = {
-                {"1", "2", "3", "a"},
-                {"5", "6", "7", "8"},
+        
+        String[][] wrongSizeArray = {
+                {"1", "2", "3"},
+                {"4", "5", "6"},
+                {"7", "8", "9"}
+        };
+
+        String[][] wrongDataArray = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "seven", "8"},
                 {"9", "10", "11", "12"},
                 {"13", "14", "15", "16"}
         };
-        String [][] invalidSizeArray = {
-                {"1", "2", "3"},
-                {"5", "6", "7"}
-        };
-        try {
-            System.out.println("Сумма элементов корректного массива: " + workWithArray(correctArray))
-        } catch (IllegalArgumentException e) {
-            System.out.println("Ошибка: " + e.getMessage()");
-        }
-        try {
-            System.out.println("Сумма элементов некорректного массива: " + workWithArray(incorrectArray));
-        } catch (IllegalArgumentException e) {
-            System.out.println("Ошибка: " + e.getMessage());
-        }
-        try {
-            System.out.println("Сумма элементов массива неверного размера: " + workWithArray(invalidSizeArray));
-        } catch (IllegalArgumentException e) {
-            System.out.println("Ошибка: " + e.getMessage());
-        }
-        }
-    generateArrayIndexOutOfBounds();
 
-    public static int workWithArray (String [][] array) {
-        if (array.length != 4 || array[0].length != 4) {
-            throw new IllegalArgumentException("Размер массива должен быть 4x4");
+        try {
+            System.out.println("Сумма элементов: " + sumArray(array));
+        } catch (MyArraySizeException e) {
+            System.out.println("Ошибка размера массива: " + e.getMessage());
+        } catch (MyArrayDataException e) {
+            System.out.println("Ошибка данных: " + e.getMessage());
         }
+
+        System.out.println("\n--- Тест ArrayIndexOutOfBoundsException ---");
+        testArrayIndexOutOfBounds();
+
+        System.out.println("\n--- Тест с массивом неверного размера ---");
+        try {
+            sumArray(wrongSizeArray);
+        } catch (MyArraySizeException e) {
+            System.out.println("MyArraySizeException: " + e.getMessage());
+        } catch (MyArrayDataException e) {
+            System.out.println("MyArrayDataException: " + e.getMessage());
+        }
+
+        System.out.println("\n--- Тест с неверными данными ---");
+        try {
+            sumArray(wrongDataArray);
+        } catch (MyArraySizeException e) {
+            System.out.println("MyArraySizeException: " + e.getMessage());
+        } catch (MyArrayDataException e) {
+            System.out.println("MyArrayDataException: " + e.getMessage());
+        }
+    }
+
+    public static int sumArray(String[][] array) throws MyArraySizeException, MyArrayDataException {
+        if (array.length != 4) {
+            throw new MyArraySizeException("Размер массива по строкам должен быть 4, а является: " + array.length);
+        }
+
         int sum = 0;
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].length != 4) {
+                throw new MyArraySizeException(
+                        String.format("Размер строки %d должен быть 4, а является: %d", i, array[i].length)
+                );
+            }
+
+            for (int j = 0; j < array[i].length; j++) {
                 try {
                     sum += Integer.parseInt(array[i][j]);
                 } catch (NumberFormatException e) {
-                    throw new IllegalArgumentException("Неверные данные в ячейке [" + i + "][" + j + "]: " + array[i][j]);
+                    throw new MyArrayDataException(
+                            String.format("Неверные данные в ячейке [%d][%d]: '%s'", i, j, array[i][j])
+                    );
                 }
             }
         }
         return sum;
     }
 
-    private static void generateArrayIndexOutOfBounds() {
-        int[] arr = new int[5];
+    public static void testArrayIndexOutOfBounds() {
+        int[] numbers = {1, 2, 3, 4, 5};
+
         try {
-            System.out.println(arr[10]);
+            System.out.println("Число по индексу 10: " + numbers[10]);
         } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Ошибка: " + e.getMessage());
+            System.out.println("ArrayIndexOutOfBoundsException перехвачено: " + e.getMessage());
+            System.out.println("Размер массива: " + numbers.length);
+            System.out.println("Индексы доступны: 0-" + (numbers.length - 1));
+        }
+
+        String[][] matrix = new String[3][3];
+        try {
+            matrix[5][5] = "test";
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("ArrayIndexOutOfBoundsException в двумерном массиве: " + e.getMessage());
         }
     }
 }
---
+++ AstonLearning_5/src/MyArrayDataException.java
@@ -0,0 +1,3 @@
+class MyArrayDataException extends Exception {
+    public MyArrayDataException(String message) { super(message); }
+}
